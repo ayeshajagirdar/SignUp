5e971afe29a4d9f32969092b6f946c4d5d85008e
ayeshajagirdar <ayeshajagirdarm88@gmail.com>
2021-06-17 20:18:39 +0530

    SignupLogin

--- app/src/main/java/com/example/firebase/LoginActivity.java
+++ app/src/main/java/com/example/firebase/LoginActivity.java
@@ -13,8 +13,8 @@ import java.util.PrimitiveIterator;
 
 public class LoginActivity extends AppCompatActivity {
     private TextView login;
-    private EditText inputEmail;
-    private EditText inputPassword;
+    private EditText Email;
+    private EditText Password;
     private TextView forgotPassword;
     private Button btnlogin;
     private TextView textViewSignUp;
@@ -25,6 +25,10 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         TextView btn = findViewById(R.id.textViewSignUp);
+        login = findViewById(R.id.logo);
+        Email = findViewById(R.id.inputEmail);
+        Password = findViewById(R.id.inputEmail);
+        forgotPassword = findViewById(R.id.forgotPassword);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
--- app/src/main/java/com/example/firebase/RegisterActivity.java
+++ app/src/main/java/com/example/firebase/RegisterActivity.java
@@ -2,13 +2,38 @@ package com.example.firebase;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
+    private EditText UserName;
+    private EditText Email;
+    private EditText Password;
+    private EditText ConfirmPassword;
+    private TextView register;
+    private Button btnRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        TextView btn = findViewById(R.id.alreadyHaveAccount);
+        UserName = findViewById(R.id.inputUserName);
+        Email = findViewById(R.id.inputEmail);
+        Password = findViewById(R.id.inputPassword);
+        ConfirmPassword = findViewById(R.id.inputConfirmPassword);
+        register = findViewById(R.id.logo);
+        btnRegister = findViewById(R.id.btnRegister);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(RegisterActivity.this , LoginActivity.class));
+            }
+        });
     }
 }
